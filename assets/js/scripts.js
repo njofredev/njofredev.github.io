6f38233d248d119c87a97ef9ce80de3e6c9c388a
njofredev <nico.jofreandrade@gmail.com>
2024-06-05 17:38:01 -0400

    Se añade pequeña función de nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,12 @@ const menu = document.getElementById('menu');
 menuButton.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
